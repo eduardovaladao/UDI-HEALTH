fee3cde5607d18e6eab18b6b4955951b06dc5e0a
Eduardo Carvalho Valadão <104527380+eduardovaladao@users.noreply.github.com>
2024-09-05 21:32:39 -0300

    Criando a base do projeto com SpringBoot
    A aplicação já pode rodar páginas do tipo JSP e lançar um servidor na máquina local com o Tomcat.
    Um "Hello, World!" vai aparecer na tela após pesquisar no navegador por "localhost:8080"

---
+++ udi-health/src/main/java/com/application/udi_health/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.application.udi_health.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping; 
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
