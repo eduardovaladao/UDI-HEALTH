90def45eace109d3e580b7d809dff82dc00d6f98
Eduardo Carvalho Valadão <104527380+eduardovaladao@users.noreply.github.com>
2024-10-07 17:21:22 -0300

    Add files via upload

---
+++ src/main/java/com/udihealth/UdiHealthApplication.java
@@ -0,0 +1,13 @@
+package com.udihealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdiHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdiHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udihealth/UdiHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udihealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdiHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
